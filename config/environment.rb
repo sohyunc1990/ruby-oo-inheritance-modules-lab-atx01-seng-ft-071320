283b02b9b5c6821cae0bb25dcf8082d3876a7ecc
Sohyun Lee <sohyunc1990@gmail.com>
2020-07-26 23:44:33 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 require_relative '../lib/concern/findable'
  require_relative '../lib/concern/memorable.rb'
-# require_relative '../lib/concerns/paramable'
+require_relative '../lib/concern/paramable'
 
 require_relative '../lib/artist.rb'
 require_relative '../lib/song.rb'
