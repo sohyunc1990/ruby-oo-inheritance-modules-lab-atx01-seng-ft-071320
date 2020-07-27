a0663e156865158b4bacda77a571017b24aed7e4
Sohyun Lee <sohyunc1990@gmail.com>
2020-07-27 00:15:52 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,8 @@
 require 'pry'
 
-# require_relative '../lib/concerns/findable'
-# require_relative '../lib/concerns/memorable'
-# require_relative '../lib/concerns/paramable'
+require_relative '../lib/concern/findable'
+ require_relative '../lib/concern/memorable.rb'
+require_relative '../lib/concern/paramable'
 
 require_relative '../lib/artist.rb'
 require_relative '../lib/song.rb'
